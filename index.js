ce2a66b74f61590eebff41c212d27bf893fe1efd
Ravelo Meva Soavina <mevaravelosoavina@gmail.com>
2021-08-03 21:35:41 +0300

    Implementation of the folder structure fondation

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const methodOverride = require("method-override");
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+const clientRoutes = require("./routes/client.route")
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -16,6 +18,6 @@ app.use(methodOverride("_method"));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', ()=> console.log("appelle"))
+app.use('/api/v1/client', clientRoutes)
 
 app.listen(PORT , () => {console.log(`The server is up and running on ${PORT}`)});
